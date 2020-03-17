9235dde0700ceae25da6e257ae5f1f614c5ef720
Diego Rodrigues Felisbino <diego.felisbino@santosbrasil.com.br>
2020-03-17 11:52:16 -0300

    Treinamento no serviço

---
+++ TestDrive/TestDrive/TestDrive/Views/DetalheView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestDrive.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetalheView : ContentPage
+    {
+        public Veiculo Veiculo { get; set; }
+        public DetalheView(Veiculo veiculo)
+        {
+            InitializeComponent();            
+            Veiculo = veiculo;
+            this.BindingContext = this;
+        }
+
+        private void buttonProximo_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AgendamentoView(Veiculo));
+        }
+    }
+}
